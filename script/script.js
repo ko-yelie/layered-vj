54048090c51a1025ef7a0965186956a2a5c40fd9
Ko.Yelie <ko.yelie@gmail.com>
2018-04-22 17:06:21 +0900

    FIx warning

--- script/script.js
+++ script/script.js
@@ -363,6 +363,16 @@ function initControl() {
 }
 
 function init() {
+  // video texture
+  var videoTexture = gl.createTexture(gl.TEXTURE_2D)
+  gl.activeTexture(gl.TEXTURE0)
+  gl.bindTexture(gl.TEXTURE_2D, videoTexture)
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+
   // textures
   gl.activeTexture(gl.TEXTURE0 + VIDEO_BUFFER_INDEX)
   gl.bindTexture(gl.TEXTURE_2D, videoFramebuffers[0].texture)
@@ -394,16 +404,6 @@ function init() {
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.drawElements(gl.TRIANGLES, planeIndex.length, gl.UNSIGNED_SHORT, 0)
   }
-
-  // video texture
-  var videoTexture = gl.createTexture(gl.TEXTURE_2D)
-  gl.activeTexture(gl.TEXTURE0)
-  gl.bindTexture(gl.TEXTURE_2D, videoTexture)
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 
   // reset picture
   gl.useProgram(picturePrg.program)
