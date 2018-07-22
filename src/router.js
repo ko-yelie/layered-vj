0a72da93a40908d4a584aebc4f6fc22d2dd5c840
ko-yelie <ko.yelie@gmail.com>
2018-07-22 20:06:13 +0900

    Change router mode to HTML5 history mode

--- src/router.js
+++ src/router.js
@@ -1,19 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
+import Home from './views/Home.vue'
 import Control from './control/Control.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
     {
       path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/control',
       name: 'control',
       component: Control
     }
