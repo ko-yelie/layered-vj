f8bb84b17094a88595f504dacb6b843f7d9ba83b
ko-yelie <ko.yelie@gmail.com>
2018-07-24 00:55:47 +0900

    Add pop-up note

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,9 @@
   h1.md-display-4 Layered VJ
   router-link(to="/control")
     md-button.md-raised.md-primary Go to Control
-  p: strong.md-body-2 Note: The new tab will automatically open on the control page.
+  p: strong.md-body-2 Note: The pop-up for visual will automatically open on the control page.
+    br
+    | Please allow pop-ups for this site.
 </template>
 
 <style lang="scss">
@@ -18,6 +20,7 @@ body {
   align-items: center;
   position: relative;
   height: 100%;
+  text-align: center;
 }
 
 h1 {
